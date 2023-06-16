76507460a2865440c76fb64ac18bb80c53a61eb9
JUAN SARA <juan.sara@utec.edu.pe>
2023-06-15 20:22:50 -0500

    cronjob partial

--- notificaciones.py
+++ notificaciones.py
@@ -56,30 +56,13 @@ async def send_notification_noche(hora):
 
 
 # TEMPRANO
-schedule.every().day.at("06:00").do(asyncio.run, send_notification_temprano("6:00 AM"))
-schedule.every().day.at("07:00").do(asyncio.run, send_notification_temprano("7:00 AM"))
-schedule.every().day.at("08:00").do(asyncio.run, send_notification_temprano("8:00 AM"))
-schedule.every().day.at("09:00").do(asyncio.run, send_notification_temprano("9:00 AM"))
-schedule.every().day.at("10:00").do(asyncio.run, send_notification_temprano("10:00 AM"))
 schedule.every().day.at("11:00").do(asyncio.run, send_notification_temprano("11:00 AM"))
 
 # TARDE
-schedule.every().day.at("13:00").do(asyncio.run, send_notification_tarde("13:00 PM"))
-schedule.every().day.at("14:00").do(asyncio.run, send_notification_tarde("14:00 PM"))
-schedule.every().day.at("15:00").do(asyncio.run, send_notification_tarde("15:00 PM"))
-schedule.every().day.at("16:00").do(asyncio.run, send_notification_tarde("16:00 PM"))
 schedule.every().day.at("17:00").do(asyncio.run, send_notification_tarde("17:00 PM"))
 
 # NOCHE
-schedule.every().day.at("19:00").do(asyncio.run, send_notification_noche("19:00 PM"))
-schedule.every().day.at("20:06").do(asyncio.run, send_notification_noche("20:00 PM"))
-schedule.every().day.at("21:00").do(asyncio.run, send_notification_noche("21:00 PM"))
-schedule.every().day.at("22:00").do(asyncio.run, send_notification_noche("22:00 PM"))
-schedule.every().day.at("23:00").do(asyncio.run, send_notification_noche("23:00 PM"))
 schedule.every().day.at("00:00").do(asyncio.run, send_notification_noche("00:00 AM"))
-schedule.every().day.at("01:00").do(asyncio.run, send_notification_noche("1:00 AM"))
-schedule.every().day.at("02:00").do(asyncio.run, send_notification_noche("2:00 AM"))
-schedule.every().day.at("03:00").do(asyncio.run, send_notification_noche("3:00 AM"))
 
 
 if __name__ == '__main__':
